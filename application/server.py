f86f80b33eb8e469eaba7d0e153ce1d338f5153d
Alan James Américo <alanjamesamerico@gmail.com>
2017-11-04 15:28:49 -0300

    Arrumando rotas. 9 Tentativa

--- application/server.py
+++ application/server.py
@@ -34,7 +34,7 @@ application = Application([
 ])
 
 def server_prod():
-    application.listen(PORT, URL_PROD)
+    application.listen(URL_PROD)
     IOLoop.instance().start()
 
 def server_local():
