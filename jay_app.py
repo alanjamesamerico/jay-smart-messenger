47c225544c9283fd2ff807a9407f4ace7cc3dd39
Alan James Américo <alanjamesamerico@gmail.com>
2017-11-04 15:19:54 -0300

    Arrumando rotas. 8 Tentativa

--- jay_app.py
+++ jay_app.py
@@ -1,7 +1,7 @@
 from application.server import server_prod, server_local
 from application.im.telegram.telegram_message_handler import TelegramMessageHandler
 
-#server_prod()
-server_local()
+server_prod()
+#server_local()
 #telegram = TelegramMessageHandler()
 #telegram.run_bot()
